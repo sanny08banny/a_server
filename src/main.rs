a3f096f2b5ccebe14716cd2a58755fe10c6dcefe
githinjihans <hanson4e@gmail.com>
2023-09-12 13:25:27 +0300

    one big commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use axum::{
+    extract::{Json, Path},
+    routing::{get, Router},
+};
+use hyper::{Body, Response};
+use serde_json::Value;
+use std::{
+    fs::{self, File},
+    io::{BufReader, Read},
+    net::SocketAddr,
+};
+use tower_http::cors::CorsLayer;
+
+#[tokio::main]
+async fn main() {
+    let addr = "0.0.0.0:4000";
+    let app = Router::new()
+        .route("/houses", get(handler))
+        .route("/houses/:path", get(image_handler))
+        .route("/houses/download/:path", get(download_img))
+        .layer(CorsLayer::permissive());
+    axum::Server::bind(&addr.trim().parse().expect("Invalid address"))
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap();
+}
+async fn handler() -> Json<Value> {
+    let y = fs::read_to_string("test.json").expect("json file not found");
+    let x: Value = serde_json::from_str(&y).expect("invalid json");
+    Json(x)
+}
+
+async fn image_handler(path: Path<String>) -> Response<Body> {
+    let h = File::open(path.0).expect("file not found");
+    let mut buf_reader = BufReader::new(h);
+    let mut contents = Vec::new();
+    buf_reader.read_to_end(&mut contents).unwrap();
+    Response::new(Body::from(contents))
+}
+
+async fn download_img(path: Path<String>) -> Response<Body> {
+    let h = File::open(path.0.clone()).expect("file not found");
+    let mut buf_reader = BufReader::new(h);
+    let mut contents = Vec::new();
+    buf_reader.read_to_end(&mut contents).unwrap();
+    let attachment = format!("attachment; filename={}", path.0);
+    Response::builder()
+        .header("Content-Disposition", attachment)
+        .body(Body::from(contents))
+        .unwrap()
+}
