f19fb4e1854125346a40ce1f87d3a0b9c903dc50
GithinjiHans <hanson4e@gmail.com>
2023-10-02 22:21:56 +0300

    add pass key

--- src/payment_gateway/merchant_portal.rs
+++ src/payment_gateway/merchant_portal.rs
@@ -8,7 +8,7 @@ pub struct Merchant<'a> {
 impl Merchant<'_> {
     pub fn get_credentials() -> Self {
         Merchant {
-            pass_key: "Enter your pass key here".to_owned(),
+            pass_key: "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919".to_owned(),
             business_short_code: 174379,
             basic_auth:
                 "Basic InthISf0rMatsWqKjgafiyu3jbkauyi32jhbjtg89dakbjbahsd",
