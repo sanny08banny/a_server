db691727bb9a1a0aeaaae5d3a181f91889646cef
GithinjiHans <hanson4e@gmail.com>
2023-12-20 17:13:10 +0300

    .

--- src/fcm_t/token.rs
+++ src/fcm_t/token.rs
@@ -8,6 +8,7 @@ pub async fn update_token(h:Path<(String,String)>) -> Result<StatusCode, StatusC
     let user_id = h.0.0;
     let token = h.0.1;
     let db = db_client().await;
+    println!("{} {}", user_id, token);
     let query = format!("UPDATE users SET notification_token='{}' WHERE user_id='{}'", token, user_id);
     db.execute(query.as_str(), &[]).await.unwrap();
     Ok(StatusCode::OK)
