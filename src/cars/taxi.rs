bd8370a2386421616c984a2125ba9bd681477041
GithinjiHans <hanson4e@gmail.com>
2024-07-20 17:41:11 +0300

    update if fully verified

--- src/cars/taxi.rs
+++ src/cars/taxi.rs
@@ -21,11 +21,20 @@ pub async fn init_taxi(db: State<DbClient>, taxi: Json<Taxi>) -> String {
 	let taxi = taxi.0;
 	let taxi_id = ecryption_engine::CUSTOM_ENGINE.encode(format!("{}{}{}{}", taxi.driver_id, taxi.plate_number, taxi.model, taxi.color));
 
-	let statement = "INSERT INTO taxi (taxi_id,driver_id,model,color,plate_number,category,manufacturer) VALUES ('$1','$2','$3','$4','$5','$6','$7')";
+	let mut statement = "INSERT INTO taxi (taxi_id,driver_id,model,color,plate_number,category,manufacturer,verified) VALUES ('$1','$2','$3','$4','$5','$6','$7',$8)";
 	db.execute(
 		statement,
-		&[&taxi_id, &taxi.driver_id, &taxi.model, &taxi.color, &taxi.plate_number, &taxi.category, &taxi.manufacturer],
+		&[&taxi_id, &taxi.driver_id, &taxi.model, &taxi.color, &taxi.plate_number, &taxi.category, &taxi.manufacturer,&false],
 	)
+	.await
+	.unwrap();
+	statement="INSERT INTO taxi_verifications (driver_id,inspection_report
+	insurance,
+	driving_licence,
+	psv_licence,
+	national_id) VALUES ('$1',$2,$3,$4,$5,$6)";
+	db.execute(
+	statement,&[&taxi.driver_id,&false,&false,&false,&false,&false])
 	.await
 	.unwrap();
 
@@ -61,8 +70,9 @@ async fn get_unverified_taxis(db: State<DbClient>)->Json<Vec<Row>>{
 	  return Json(drivers);
 }
 
+// accessible to both the driver and admin
 async fn get_unverified_documents(db: State<DbClient>,driver_id:String)->Value{
-	let verification_documents=db.query_opt("SELECT * FROM taxi_verifications WHERE email=$1", &[&driver_id]).await.unwrap();
+	let verification_documents=db.query_opt("SELECT * FROM taxi_verifications WHERE driver_id=$1", &[&driver_id]).await.unwrap();
 	match  verification_documents {
 		Some(row) => {
 			let docs=vec!["national_id","insurance","driving_licence","psv_licence","inspection_report"];
@@ -71,6 +81,12 @@ async fn get_unverified_documents(db: State<DbClient>,driver_id:String)->Value{
 				if !row.get::<_,bool>(doc){
 					unverified.push(doc);
 				}
+			}
+			// check if verification is complete and update status
+			if unverified.len()==0{
+				db.execute("UPDATE taxi SET verified=true WHERE driver_id=$1", &[&driver_id])
+				.await
+				.unwrap();
 			}
             json!(unverified)
 		},
@@ -105,10 +121,10 @@ async fn get_unverified_document(req: VerificationObject)->Body{
 
 
 async fn verify_document(db: State<DbClient>, req:VerificationObject)->StatusCode{
-	let mut modified:u64;
+	let modified:u64;
 	match req.document_type {
 		VerificationDocumentType::NationalId => { modified=db.0.execute("UPDATE taxi_verifications SET national_id=true WHERE driver_id=$1", &[&req.driver_id]).await.unwrap()},
-		VerificationDocumentType::Insurance => {{ modified=db.0.execute("UPDATE taxi_verifications SET insurance=true WHERE driver_id=$1", &[&req.driver_id]).await.unwrap()}},
+		VerificationDocumentType::Insurance => {modified=db.0.execute("UPDATE taxi_verifications SET insurance=true WHERE driver_id=$1", &[&req.driver_id]).await.unwrap()},
 		VerificationDocumentType::DrivingLicence => { modified=db.0.execute("UPDATE taxi_verifications SET driving_licence=true WHERE driver_id=$1", &[&req.driver_id]).await.unwrap()},
 		VerificationDocumentType::PSVLicence => { modified=db.0.execute("UPDATE taxi_verifications SET psv_licence=true WHERE driver_id=$1", &[&req.driver_id]).await.unwrap()},
 		VerificationDocumentType::InspectionReport => { modified=db.0.execute("UPDATE taxi_verifications SET inspection_report=true WHERE driver_id=$1", &[&req.driver_id]).await.unwrap()},
